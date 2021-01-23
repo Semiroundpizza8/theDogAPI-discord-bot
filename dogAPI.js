f029f3c8e866e0a35044904458e74ad3b3836ffe
Benjamin Odisho <Semiroundpizza8@gmail.com>
2021-01-23 09:04:17 -0600

    Removed all non-boilerplate code

--- dogAPI.js
+++ dogAPI.js
@@ -1,11 +1,8 @@
-const querystring = require("querystring");
-const axios = require("axios");
-
 const dogAPI = {
   /**
    * Makes a request to theDogAPI.com for a random dog image with breed info attached.
    */
-  getDog: async function (sub_id, DOG_API_URL, DOG_API_KEY) {
+  getDog: async function (/* */) {
     var query_params = {
       has_breeds: true,
       mime_types: "jpg,png",
@@ -15,20 +12,13 @@ const dogAPI = {
     };
 
     // convert this object to query string
-    let queryString = querystring.stringify(query_params);
 
     try {
+
       // construct the API Get request url
-      let _url = DOG_API_URL + `v1/images/search?${queryString}`;
 
-      // make the request passing the url, and headers object which contains the API_KEY
-      const response = await axios.get(_url, {
-        headers: {
-          "X-API-KEY": DOG_API_KEY,
-        },
-      });
-
-      return response.data[0];
+      // make the request to the actual API
+      
     } catch (e) {
       console.log(e);
     }
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
-require('dotenv').config()
-
 const Discord = require('discord.js');
-const dogAPI = require('./dogAPI');
 
 // Discord connection code ---
 const client = new Discord.Client();
@@ -13,24 +10,17 @@ client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 // When bot sees new message in channel....
 client.on('message' ,async message => {
   // If the content isn't what we react to, ignore it
-  if (message.content !== 'woof') return;
 
   try {
     // Grab the image from the dog API    
-    var dogInfo = await dogAPI.getDog(
-      message.author.username,
-      process.env.DOG_API_URL,
-      process.env.DOG_API_KEY
-    );
 
     
     // Send a formatted message to the discord channel
-    var breed = dogInfo.breeds[0];
-    const messageText = `***${breed.name}*** \r *${breed.temperament}*`;
+    const messageText = `***DOG BREED*** \r *DOG TEMPERMENT*`;
     
     message.channel.send(
       messageText,
-      { files: [dogInfo.url] }
+      { files: [/* */] }
     );
     
     // if you didn't want to see the text, just send the file
