4b66c4c698367899234df2de148774c659f0955c
Benjamin Odisho <Semiroundpizza8@gmail.com>
2021-01-23 13:21:43 -0600

    In-Class Solution

--- dogAPI.js
+++ dogAPI.js
@@ -1,16 +1,39 @@
+const querystring = require("querystring");
+const axios = require("axios");
+
 const dogAPI = {
   /**
    * Makes a request to theDogAPI.com for a random dog image with breed info attached.
    */
-  getDog: async function (/* Take in API keys + whatever else is needed */) {
+  getDog: async function (sub_id, DOG_API_URL, DOG_API_KEY) {
     // Initialize object with query settings
+    var query_params = {
+      has_breeds: true,
+      mime_types: "jpg,png",
+      size: "small",
+      sub_id: sub_id,
+      limit: 1,
+    };
 
     try {
-
       // construct the API Get request url
+      
+      const query = querystring.stringify(query_params);
+      // "?has_breeds=true&mime_types=jpeg%20png....."
+      
+      const url = `${DOG_API_URL}v1/images/search?${query}`
 
       // make the request to the actual API
+      let response = await axios.get(
+        url,
+        {
+          header: {
+            "X-API-KEY": DOG_API_KEY
+          }
+        }
+      )
       
+      return response.data
     } catch (e) {
       console.log(e);
     }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
+require('dotenv').config();
+
 const Discord = require('discord.js');
+const dogAPI = require('./dogAPI');
 
 // Discord connection code ---
 const client = new Discord.Client();
@@ -8,24 +11,37 @@ const client = new Discord.Client();
 client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 
 // When bot sees new message in channel....
-client.on('message' ,async message => {
+client.on('message' , async message => {
   // If the content isn't what we react to, ignore it
+  if(message.content === "woof") {
+    try {
+      // Grab the image from the dog API
+      var dogInfo = await dogAPI.getDog(
+        message.author.username,
+        process.env.DOG_API_URL,
+        process.env.DOG_API_KEY,
+      );
 
-  try {
-    // Grab the image from the dog API    
+      stripe.payments.create(
+        paymentObj,
+        process.env.STRIPE_KEY,
+        process.env.STRIPE_URL
+      )
 
-    
-    // Send a formatted message to the discord channel
-    const messageText = `***DOG BREED*** \r *DOG TEMPERMENT*`;
-    
-    message.channel.send(
-      messageText,
-      { files: [/* */] }
-    );
-  } catch (error) {
-    console.log(error);
+      let breed = dogInfo[0].breeds[0];
+
+      // Send a formatted message to the discord channel
+      const messageText = `***${breed.name}*** \r *${breed.temperament}*`;
+      
+      message.channel.send(
+        messageText,
+        { files: [dogInfo[0].url] }
+      );
+
+    } catch (error) {
+      console.log(error);
+    }
   }
-
 });
 
 // When bot breaks....
